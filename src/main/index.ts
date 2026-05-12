df7905cb82016a504db4d8ed4dadca20ebbbb729
Reese.Y <yefulincode@gmail.com>
2026-05-12 17:35:08 +0800

    修改应用icon

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,6 +11,7 @@ function createWindow() {
     minWidth: 800,
     minHeight: 600,
     title: 'Modraw',
+    icon: join(__dirname, '../../build/icon.png'),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       contextIsolation: true,
